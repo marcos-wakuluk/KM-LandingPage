5ae27034563970225045b0b92577c97fccc154fc
Marcos <marcos.wakuluk@wuala.net>
2024-09-22 22:27:20 -0300

    spelling mistakes corrected

--- src/components/sections/Section4.jsx
+++ src/components/sections/Section4.jsx
@@ -8,29 +8,17 @@ const Section4 = () => {
       <div className="section4-container flex flex-col md:flex-row bg-custom-black">
         <div className="content-container md:w-1/2 w-full text-white py-4 px-8 relative">
           <h2 className="text-3xl font-bold mb-2 uppercase mt-20">
-            Imaginate tener todo en un sólo lugar Si sabes lo que queres lograr
-            pero no sabes exactamente como hacerlo, vas a seguir perdiendo el
-            tiempo
+            Imaginate tener todo en un sólo lugar Si sabes lo que queres lograr pero no sabes exactamente como hacerlo, vas a seguir perdiendo el tiempo
           </h2>
           <h3 className="text-2xl font-semibold mb-4 mt-20">
-            Nuestra asesoria/membresia funciona como una subscrcion mensual, una
-            vez dentro, recibiras un plan de entrenamiento y alimentacion 100%
-            medida que se akustara mes ame ssegun tus progresos y nuestras
-            consideraciones. contaras tambien con apoyo diario en un grupo
-            activo, exclusivo para miembros y mi wsp peseonal para cuando lo
-            necesites.
+            Nuestra asesoria/membresia funciona como una suscripción mensual, una vez dentro, recibiras un plan de entrenamiento y alimentacion 100% medida que
+            se ajustará mes a mes según tus progresos y nuestras consideraciones. contarás también con apoyo diario en un grupo activo, exclusivo para miembros
+            y mi wsp personal para cuando lo necesites.
           </h3>
-          <ScrollToButton
-            text="Quiero unirme"
-            className="text-2xl border-2 border-blue-500 mt-20 mb-20"
-          />
+          <ScrollToButton text="Quiero unirme" className="text-2xl border-2 border-blue-500 mt-20 mb-20" />
         </div>
         <div className="image-container w-full md:w-1/2 hidden md:block">
-          <img
-            src={lucasBottomless1}
-            alt="Logo"
-            className="w-full h-auto object-cover"
-          />
+          <img src={lucasBottomless1} alt="Logo" className="w-full h-auto object-cover" />
         </div>
       </div>
     </>
--- src/components/sections/Section6.jsx
+++ src/components/sections/Section6.jsx
@@ -4,27 +4,27 @@ const Section6 = () => {
   return (
     <div className="flex bg-custom-black justify-center items-center py-12">
       <div className="w-full max-w-3xl p-8 rounded-lg bg-custom-gray shadow-lg">
-        <h2 className="text-3xl font-bold text-center text-white mb-8">
-          Encuentra todo acá:
-        </h2>
-        <ul className="text-white space-y-6">
-          <li className="flex items-center text-lg">
-            <CheckCircleIcon className="h-6 w-6 text-green-500 mr-4" />
-            <span>Entrenamiento y nutrición</span>
-          </li>
-          <li className="flex items-center text-lg">
-            <CheckCircleIcon className="h-6 w-6 text-green-500 mr-4" />
-            <span>Videos y alternativas</span>
-          </li>
-          <li className="flex items-center text-lg">
-            <CheckCircleIcon className="h-6 w-6 text-green-500 mr-4" />
-            <span>Seguimiento y soporte</span>
-          </li>
-          <li className="flex items-center text-lg">
-            <CheckCircleIcon className="h-6 w-6 text-green-500 mr-4" />
-            <span>Comunidad fitness</span>
-          </li>
-        </ul>
+        <h2 className="text-3xl font-bold text-center text-white mb-8">Encuentra todo acá:</h2>
+        <div className="flex justify-center">
+          <ul className="text-white space-y-6">
+            <li className="flex items-center text-lg">
+              <CheckCircleIcon className="h-6 w-6 text-green-500 mr-4" />
+              <span>Entrenamiento y nutrición</span>
+            </li>
+            <li className="flex items-center text-lg">
+              <CheckCircleIcon className="h-6 w-6 text-green-500 mr-4" />
+              <span>Videos y alternativas</span>
+            </li>
+            <li className="flex items-center text-lg">
+              <CheckCircleIcon className="h-6 w-6 text-green-500 mr-4" />
+              <span>Seguimiento y soporte</span>
+            </li>
+            <li className="flex items-center text-lg">
+              <CheckCircleIcon className="h-6 w-6 text-green-500 mr-4" />
+              <span>Comunidad fitness</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
