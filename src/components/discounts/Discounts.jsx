182c9dc3fed17f3e6b2b5b7250551a37f843d3ca
Marcos <marcos.wakuluk@wuala.net>
2024-10-26 14:43:45 -0300

    changes in Discount component

--- src/components/discounts/Discounts.jsx
+++ src/components/discounts/Discounts.jsx
@@ -4,11 +4,13 @@ import ScrollToButton from "../sections/ScrollToButton";
 const Discount = () => {
   const [minutes, setMinutes] = useState(15);
   const [seconds, setSeconds] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (minutes === 0 && seconds === 0) {
         clearInterval(interval);
+        setIsVisible(false);
       } else if (seconds === 0) {
         setMinutes((prevMinutes) => prevMinutes - 1);
         setSeconds(59);
@@ -16,21 +18,22 @@ const Discount = () => {
         setSeconds((prevSeconds) => prevSeconds - 1);
       }
     }, 1000);
+
     return () => clearInterval(interval);
   }, [minutes, seconds]);
 
+  if (!isVisible) return null;
+
   return (
-    <>
-      <div className="text-center bg-blue-500 p-2 w-full desktop-sticky">
-        <div className="flex flex-col md:flex-row justify-around items-center bg-blue-500 text-black p-2 md:p-8">
-          <h3 className="text-lg md:text-2xl font-bold uppercase mb-2 md:mb-0">Últimos cupos</h3>
-          <h3 className="text-lg md:text-2xl font-semibold mb-2 md:mb-0">
-            {minutes} minutos {seconds} segundos
-          </h3>
-          <ScrollToButton text="Quiero unirme" className="text-lg md:text-2xl" />
-        </div>
+    <div className="text-center bg-blue-500 p-2 w-full desktop-sticky">
+      <div className="flex flex-col md:flex-row justify-around items-center bg-blue-500 text-black p-2 md:p-8">
+        <h3 className="text-lg md:text-2xl font-bold uppercase mb-2 md:mb-0">Últimos cupos</h3>
+        <h3 className="text-lg md:text-2xl font-semibold mb-2 md:mb-0">
+          {minutes} minutos {seconds} segundos
+        </h3>
+        <ScrollToButton text="Quiero unirme" className="text-lg md:text-2xl" />
       </div>
-    </>
+    </div>
   );
 };
 
