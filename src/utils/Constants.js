16586dce0bd2120f62669e20013d9118c4d74198
Marcos <marcos.wakuluk@wuala.net>
2025-02-11 20:54:53 -0300

    Refactor questions and responses in Constants.js for clarity and detail enhancement

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -110,22 +110,22 @@ export const Questions = [
   {
     question: "¿En qué nos diferenciamos de otros programas?",
     response:
-      "Resultados, profesionalismo, eficiencia, eficacia, experiencia en carne propia, atención personalizada y acompañamiento son puntos clave en los que nos sentimos muy identificados. Como integrante del team de kmprofitness aprenderás a comer y entrenar de manera óptima para promover los cambios que tu salud necesita, para ello tendrás un equipo de profesionales a tu disposición diariamente para brindarte la ayuda necesaria; evaluaremos la ejecución de los ejercicios y lo que comes día a día si crees necesario.",
+      "Resultados, profesionalismo, eficiencia, eficacia, experiencia en carne propia, atención personalizada y acompañamiento son puntos clave en los que nos sentimos muy identificados. Como integrante del team de kmprofitness aprenderás a comer y entrenar de manera óptima para promover los cambios que tu salud necesita, para ello tendrás un equipo de profesionales a tu disposición diariamente para brindarte la ayuda necesaria; evaluaremos la ejecución de los ejercicios y lo que comes día a día si crees necesario y tendras un conjunto de herramientas que van a ser claves en tu proceso.",
   },
   {
-    question: "¿Cómo funciona?",
+    question: "¿Cómo funciona luego de unirme?",
     response:
-      "Después de realizar el pago y proporcionar tus datos, te llegara un email con acceso exclusivo a nuestra aplicación, donde encontrarás una sección llamada Primer Paso. Aquí, tendrás a tu disposición una ficha técnica para completar con tus datos personales. Este paso es crucial, ya que nos permitirá conocer todo lo necesario sobre ti para personalizar tu programa fitness de manera única. Este proceso de recopilación de información tiene una estimación de 4 días hábiles. Una vez que hayamos elaborado cuidadosamente tus planificaciones, podrás visualizarlas directamente en la pantalla principal (Home) de la aplicación desde tu dispositivo móvil. Aquí, encontrarás un enlace directo que te conectará con tu entrenador o nutricionista asignado para resolver cualquier duda que pueda surgir durante tu viaje hacia la salud y la forma física. Nuestra meta es brindarte una experiencia personalizada, eficiente y respaldada por profesionales, asegurándonos de que cada paso que tomes te acerque a tus objetivos de bienestar. ¡Estamos aquí para guiarte en cada paso del camino!",
+      "Después de realizar el pago y proporcionar tus datos, te llegara un email con acceso exclusivo a nuestra aplicación, una vez dentro de la app veras  una sección llamada METODO PPR. Aquí, encontraras diferentes fases,  a medida que avancemos iras desbloqueando. Dentro de la FASE 1 tendras  a tu disposición una ficha técnica para completar con tus datos personales. Este paso es crucial, ya que nos permitirá conocer todo lo necesario sobre ti para personalizar tu programa fitness de manera única. Este proceso de recopilación de información tiene una estimación de 4 días hábiles. Una vez que hayamos elaborado cuidadosamente tus planificaciones, podrás visualizarlas directamente en la pantalla principal de la aplicación desde tu dispositivo móvil. Aquí, encontrarás un enlace directo que te conectará con tu entrenado para resolver cualquier duda que pueda surgir durante tu viaje hacia la salud y la forma física. Nuestra meta es brindarte una experiencia personalizada, eficiente y respaldada por profesionales, asegurándonos de que cada paso que tomes te acerque a tus objetivos de bienestar. ¡Estamos aquí para guiarte en cada paso del camino!",
   },
   {
     question: "¿Cómo recibo mi programa?",
     response:
-      "Tardamos 4 días hábiles una vez que llenas el cuestionario con todos tus datos, para acceder al programa tenes que entrar en la app la cual te llega por email al inscribirte.",
+      "Lo recibirás en la app, luego de 4 dias  hábiles,  una vez que llenas el cuestionario con todos tus datos. Para acceder al programa tenes que entrar en la app la cual te llega por email al inscribirte.",
   },
   {
     question: "¿Es este un programa de suscripción?",
     response:
-      "Sí, elegí estructurarlo de esta manera para asegurar que puedas mantener y avanzar en tu progreso, ya que el mantenimiento es esencial. Ofrecemos la flexibilidad de una suscripción mensual, lo que implica actualizaciones regulares en tus programas de entrenamiento y nutrición, diseñados específicamente para ti según el plan seleccionado. Además, contarás con mi apoyo constante y el respaldo de mi equipo a lo largo de tu trayecto.",
+      "Sí, elegí estructurarlo de esta manera para asegurar que puedas mantener y avanzar en tu progreso, ya que el mantenimiento es esencial. ",
   },
   {
     question: "¿Cómo puedo cancelar mi suscripción?",
@@ -135,12 +135,12 @@ export const Questions = [
   {
     question: "¿Para quién esta creado?",
     response:
-      "Nuestro enfoque único se adapta a una variedad de audiencias: desde aquellos que dan sus primeros pasos en el mundo del fitness hasta deportistas ávidos que buscan llevar su rendimiento al siguiente nivel. Este programa no tiene límites de género ni restricciones de edad; es para hombres y mujeres de todas las edadesPara aquellos que dan sus primeros pasos, te brindamos una introducción amigable y personalizada al mundo del fitness. Si eres un deportista en busca de mejorar tu rendimiento, nuestras estrategias personalizadas te llevarán a nuevas alturas. Imagina un programa diseñado no solo para obtener resultados, sino para hacerlo de manera sostenible y adaptada a tu estilo de vida. No importa si eres principiante o experimentado, si buscas mejorar tu forma física o alcanzar metas deportivas específicas, nuestro enfoque integral se ajusta a tus necesidades. No esperes más para unirte a nuestra comunidad de transformación. ¡Eleva tu bienestar, desata tu potencial y descubre lo que realmente significa vivir en tu mejor versión! Este programa es para todos, ¡y eso incluye a TI!",
+      "Nuestro enfoque único se adapta a una variedad de audiencias: desde aquellos que dan sus primeros pasos en el mundo del fitness hasta deportistas ávidos que buscan llevar su rendimiento al siguiente nivel. Este programa no tiene límites de género ni restricciones de edad; es para hombres y mujeres de todas las edades Para aquellos que dan sus primeros pasos, te brindamos una introducción amigable y personalizada al mundo del fitness. Si eres un deportista en busca de mejorar tu rendimiento, nuestras estrategias personalizadas te llevarán a nuevas alturas. Imagina un programa diseñado no solo para obtener resultados, sino para hacerlo de manera sostenible y adaptada a tu estilo de vida. No importa si eres principiante o experimentado, si buscas mejorar tu forma física o alcanzar metas deportivas específicas, nuestro enfoque integral se ajusta a tus necesidades. No esperes más para unirte a nuestra comunidad de transformación. ¡Eleva tu bienestar, desata tu potencial y descubre lo que realmente significa vivir en tu mejor versión! Este programa es para todos, ¡y eso incluye a TI!",
   },
   {
     question: "¿Necesito experiencia previa en fitness?",
     response:
-      "No, el asesoramiento esta diseñado para todos los niveles, desde principantes hasta atletas avanzados. Adaptamos los ejercicios y la nutricion a tu nivel actual y vamos prograsando a tu ritmo",
+      "Si, Descubre la eficacia de nuestro asesoramiento fitness consultando nuestros perfiles, donde hemos cosechado más de 2000 resultados en estos 8 años de dedicación. Te garantizamos que, al seguir tus planificaciones y aprovechar la experiencia de nuestros profesionales, experimentarás resultados constantes. Confía en nuestro historial de éxito y haz de tu viaje fitness una experiencia transformadora. ¡Los resultados hablan por sí mismos!",
   },
   {
     question: "¿Funciona el asesoramiento?",
@@ -150,7 +150,7 @@ export const Questions = [
   {
     question: "¿Cómo se mide mi progreso?",
     response:
-      "Dentro de la app kmpro vas a encontrar un apartado en donde podrás dejarnos tus medidas, peso e imágenes corporales. Estos datos van a conservarse y veremos a largo plazo tus progresos. También te dejaremos una devolución y aconsejaremos.",
+      "Dentro de la app kmpro vas a encontrar un apartado en donde podrás dejarnos tus medidas, peso e imágenes corporales. Estos datos van a conservarse y veremos a largo plazo tus progresos. También te dejaremos una devolución y aconsejaremos",
   },
   {
     question: "¿Trabajan con un nutricionista?",
@@ -159,12 +159,17 @@ export const Questions = [
   {
     question: "¿Qué tipo de equipamiento necesito?",
     response:
-      "Dependiendo de tus objetivos podemos trabajar con el equipamiento que tengas diponible, sino puedes asistir a un gimnacio, podemos crear un plan para que puedas seguir desde la comodidad de tu casa o al aire libre.",
+      "Dependiendo de tus objetivos podemos trabajar con el equipamiento que tengas disponible, si no puedes asistir a un gimnasio, podemos crear un plan para que puedas seguir desde la comodidad de tu casa o al aire libre.",
   },
   {
     question: "¿Son planificaciones personalizadas?",
     response:
-      "Si, tanto las planificaciones de entrenamiento como las de alimentacion son personalizadas. Se adaptan a tus necesides, objetivos y preferencias, garantizando que se ajusten a tu estilo de vida y te ayuden a alcanzar tus metas de manera efectiva.",
+      "Si, tanto las planificaciones de entrenamiento como las de alimentación son personalizadas. Se adaptan a tus necesidades, objetivos y preferencias, garantizando que se ajusten a tu estilo de vida y te ayuden a alcanzar tus metas de manera efectiva.",
+  },
+  {
+    question: "¿Que es el metodo PPR?",
+    response:
+      "El Método PPR (Paso a Paso Real) es una metodologia para estructurar la asesoria y brindarte  organización y claridad para afrontar tu cambio, haciéndolo más didáctico y educativo. De esta manera, podés mantener la constancia y lograr cambios verdaderos.",
   },
   {
     question: "¿Que sucede si no puedo seguir el programa?",
